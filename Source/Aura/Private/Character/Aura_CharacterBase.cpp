f7c9700eda1cd0f86882219504966dae944efb66
hcdvall <henrik.sedvall@gmail.com>
2024-03-12 14:30:02 +0100

    Change directory for character base class

---
+++ Source/Aura/Private/Character/Aura_CharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Fagai Studio
+
+
+#include "Aura_CharacterBase.h"
+
+AAura_CharacterBase::AAura_CharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAura_CharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Public/Character/Aura_CharacterBase.h
@@ -0,0 +1,20 @@
+// Copyright Fagai Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Aura_CharacterBase.generated.h"
+
+UCLASS()
+class AURA_API AAura_CharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAura_CharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+	
+};
