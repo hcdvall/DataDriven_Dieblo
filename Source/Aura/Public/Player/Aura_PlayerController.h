a109740646ab45765b8bd842f2d83cf26db7dd85
hcdvall <henrik.sedvall@gmail.com>
2024-03-14 09:42:22 +0100

    Setup animation blueprints and start setting up player input

---
+++ Source/Aura/Public/Player/Aura_PlayerController.h
@@ -0,0 +1,17 @@
+// Copyright Fagai Studio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Aura_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API AAura_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
